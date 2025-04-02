edae60b3d78285a9e36587242a84678307289687
nambininaorchis <nambinina.ilaydimby@orchisintl.com>
2025-04-02 16:34:07 +0300

    edit test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,19 +6,19 @@ test.beforeEach(async ({ page }) => {
 
 test('Test 1: Ajouter un fichier, afficher les aperçus et télécharger les fichiers', async ({ page }) => {
   const chooseButton = page.locator('input[type="file"]');
-  await chooseButton.setInputFiles('./file/right.svg');
+  await chooseButton.setInputFiles('./files/right.svg');
   const imagePreview = page.locator('img[alt="Aperçu du fichier"]');
   await expect(imagePreview).toBeVisible();
   const uploadButton = page.locator('.upload-button');
   await uploadButton.click();
   page.on('dialog', async (dialog) => {
-    expect(dialog.message()).toBe('Upload réussi');
+    expect(dialog.message()).toBe('Upload réussi'); 
   });
 });
 
 test('Test 2: Ajouter un fichier, afficher les aperçus et supprimer l\'élément', async ({ page }) => {
   const chooseButton = page.locator('input[type="file"]');
-  await chooseButton.setInputFiles('./file/right.svg');
+  await chooseButton.setInputFiles('./files/right.svg');
   const imagePreview = page.locator('img[alt="Aperçu du fichier"]');
   await expect(imagePreview).toBeVisible();
   const deleteButton = page.locator('button', { hasText: 'Supprimer' });
